34ef0f97dea5004a2f9d7c16f10e09b3a4ab1583
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-03-03 16:50:31 -0800

    Attach socket to server

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
+app.use(cors({origin: '*'}))
 const server = createServer(app);
+const io = new Server(server);
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 const PORT = 3000;
-server.listen(PORT, () => {
+io.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
