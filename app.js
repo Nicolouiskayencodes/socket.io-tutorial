e8fdea9f0289e02f7f57f5392092040948ad5475
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-03-17 22:38:32 -0700

    Add message recovery through sqlite

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -18,9 +33,18 @@ io.on('connection', (socket) => {
   });
 });
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', async (msg) => {
     console.log(msg)
-    io.emit('chat message', msg);
+    let result;
+      try {
+        // store the message in the database
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        // TODO handle the failure
+        return;
+      }
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
   });
 });
 
